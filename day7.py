eb5b6acd54eb3cd1adc041b643e0cf3adabb9c30
kennedysnyder <kennedy.g.snyder@gmail.com>
2024-12-08 00:46:53 -0500

    Day 7 cleanup/simplification

--- day7.py
+++ day7.py
@@ -15,28 +15,31 @@ def is_valid(nums, operators):
 	return curr_value == ans
 
 valid_p1 = 0
-valid_p2 = 0
+def count_valid_lines():
+	valid_p1 = 0
+	valid_p2 = 0
 
-lines = data.splitlines()
-i = 1
-for line_num in range(len(lines)):
-	print(f'{i}/{len(lines)}', end='\r')
-	line = [int(x) for x in re.findall(r'\d+', lines[line_num])]
+	lines = data.splitlines()
 
-	num_operators = len(line) - 2
+	for i, line in enumerate(lines):
+		print(f'{i}/{len(lines)}', end='\r')
+		line_nums = [int(x) for x in re.findall(r'\d+', line)]
+		num_operators = len(line_nums) - 2
 
-	#all * and + combos
-	options = list(product([1,0], repeat=num_operators))
+		#all * and + combos
+		options = list(product([1,0], repeat=num_operators))
 
-	if any(is_valid(line, o) for o in options):
-		valid_p1 += line[0]
-		valid_p2 += line[0]
-		
-	else:
-		options = [o for o in product([1, 0, -1], repeat=num_operators) if -1 in o]
-		if any(is_valid(line, o) for o in options):
-			valid_p2 += line[0]
-	i += 1
-		
-print(valid_p1)
-print(valid_p2)
+		if any(is_valid(line_nums, o) for o in options):
+			valid_p1 += line_nums[0]
+			valid_p2 += line_nums[0]
+			
+		else:
+			options = [o for o in product([1, 0, -1], repeat=num_operators) if -1 in o]
+			if any(is_valid(line_nums, o) for o in options):
+				valid_p2 += line_nums[0]
+
+	return valid_p1, valid_p2
+			
+valid_p1, valid_p2 = count_valid_lines()
+print('Part 1:', valid_p1)
+print('Part 2:', valid_p2)
